e5e6af753dfcc5f13c82df98db550067aa9ca1fe
sup2is <dev.sup2is@gmail.com>
2022-02-13 23:08:28 +0900

    [CLIENT] 설정 제거

--- client/kafka/src/main/java/me/sup2is/room335/kafka/config/KafkaConsumerConfig.java
+++ client/kafka/src/main/java/me/sup2is/room335/kafka/config/KafkaConsumerConfig.java
@@ -65,25 +65,4 @@ public class KafkaConsumerConfig {
                 jsonDeserializer);
     }
 
-
-    @Bean
-    public ConsumerFactory<String, String> consumerFactory() {
-        Map<String, Object> props = new HashMap<>();
-
-        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaProperties.getBootstrapServers());
-        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
-        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
-
-        return new DefaultKafkaConsumerFactory<>(props);
-    }
-
-    @Bean
-    public ConcurrentKafkaListenerContainerFactory<String, String>
-    kafkaListenerContainerFactory() {
-        ConcurrentKafkaListenerContainerFactory<String, String> factory
-                = new ConcurrentKafkaListenerContainerFactory<>();
-        factory.setConsumerFactory(consumerFactory());
-
-        return factory;
-    }
 }
