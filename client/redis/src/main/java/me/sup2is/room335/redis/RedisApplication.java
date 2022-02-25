a6ed44d52059f8744d647ad475f2091ad8de1cb7
sup2is <dev.sup2is@gmail.com>
2022-02-25 10:25:46 +0900

    [CLIENT] redis client 모듈 추가

---
+++ client/redis/src/main/java/me/sup2is/room335/redis/RedisApplication.java
@@ -0,0 +1,12 @@
+package me.sup2is.room335.redis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RedisApplication.class);
+    }
+}
