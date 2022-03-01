f7d689a63d3d767068783a9f43f5460e65d66c03
sup2is <dev.sup2is@gmail.com>
2022-03-01 23:58:09 +0900

    [API] 객실조회시 price, id 칼럼 추가

--- api/src/main/java/me/sup2is/room335/api/room/dto/RoomDto.java
+++ api/src/main/java/me/sup2is/room335/api/room/dto/RoomDto.java
@@ -6,25 +6,31 @@ import lombok.Getter;
 import me.sup2is.room335.domain.room.RoomType;
 import me.sup2is.room335.redis.domain.room.RoomCache;
 
+import java.math.BigDecimal;
+
 public class RoomDto {
 
     @Builder
     @Getter
     @EqualsAndHashCode
     public static class Response {
+        private final Long roomId;
         private final String roomName;
         private final String roomNumber;
         private final Integer roomFloor;
         private final RoomType roomType;
         private final String description;
+        private final BigDecimal price;
 
         public static Response of(final RoomCache room) {
             return Response.builder()
+                    .roomId(room.getId())
                     .roomFloor(room.getRoomFloor())
                     .roomName(room.getRoomName())
                     .roomType(RoomType.valueOf(room.getRoomType()))
                     .roomNumber(room.getRoomNumber())
                     .description(room.getDescription())
+                    .price(room.getPrice())
                     .build();
         }
     }
